484dd16635201ea13d67affe7e7375eb0e926df1
Sedoi <sedoi.sv1@yandex.ru>
2022-12-31 05:52:53 +0700

    modified:   layout/Menu/Menu.tsx

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -3,28 +3,17 @@ import cn from 'classnames';
 import { useContext } from 'react';
 import { AppContext } from '../../context/app.context';
 import { firstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
-//import Link from 'next/link';
-//import { useRouter } from 'next/router';
-import CoursesIcon from './icons/courses.svg';
-import ServicesIcon from './icons/services.svg';
-import BooksIcon from './icons/books.svg';
-import ProductsIcon from './icons/products.svg';
-import { TopLevelCategory } from '../../interfaces/page.interfase';
-//import { firstLevelMenu } from '../../helpers/helpers';
-//import {  motion } from 'framer-motion'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { firstLevelMenu } from '../../helpers/helpers';
+import {  motion } from 'framer-motion'
 
-const firstLevelMenu: firstLevelMenuItem[] = [
-	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products }
-];
 
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
 
-	/*const router = useRouter();
+	const router = useRouter();
 
 	const variants = {
 		visible: {
@@ -52,22 +41,24 @@ export const Menu = (): JSX.Element => {
 			}
 			return m;
 		}));
-	};*/
+	};
 
 	const buildFirstLevel = () => {
 		return (
 			<>
 				{firstLevelMenu.map(m => (
 					<div key={m.route} >
-						<a href={`/${m.route}`}>
-							<div className={cn(styles.firstLevel, {
-								[styles.firstLevelActiv]: m.id == firstCategory
-							})}>
-								{m.icon}
-								<span>{m.name}</span>
-							</div>
-						</a>
-						{m.id == firstCategory}
+						<Link href={`/${m.route}`}>
+							<a >
+								<div className={cn(styles.firstLevel, {
+									[styles.firstLevelActiv]: m.id == firstCategory
+								})}>
+									{m.icon}
+									<span>{m.name}</span>
+								</div>
+							</a>
+						</Link>
+						{m.id == firstCategory && buildSecondLevel(m)}
 					</div >
 				))}
 			</>
@@ -76,30 +67,44 @@ export const Menu = (): JSX.Element => {
 
 	const buildSecondLevel = (menuItem: firstLevelMenuItem) => {
 		return (
-			<div>
-				{menu.map(m => (
-					<div key={m._id.secondCategory}>
-						<div className={styles.secondLevel}>{m._id.secondCategory}</div>
-						<div className={cn(styles.secondLevelBlock, {
-							[styles.secondLevelBlockOpened] : m.isOpened
-						})}>
-							{buildThirdLevel(m.pages, menuItem.route)}
+			<div className={styles.secondBlock}>
+				{menu.map(m => {
+					if (m.pages.map(p => p.alias).includes(router.asPath.split('/')[2])) {
+						m.isOpened = true;
+					}
+					return (
+						<div key={m._id.secondCategory}>
+							<div className={styles.secondLevel} onClick={() => openSecondLevel(m._id.secondCategory)}>{m._id.secondCategory}</div>
+							<motion.div
+								layout
+								variants={variants}
+								initial={m.isOpened ? 'visible' : 'hidden'}
+								animate={m.isOpened ? 'visible' : 'hidden'}
+								className={cn(styles.secondLevelBlock,)}
+							>
+								{buildThirdLevel(m.pages, menuItem.route)}
+							</motion.div>
 						</div>
-					</div>
-				))};
+					);
+				})}
 			</div >
 		);
 	};
 
 	const buildThirdLevel = (pages: PageItem[], route: string) => {
 		return (
-				pages.map(p => (
-					<a href={`/${route}/${p.alias}`} className={cn(styles.thirdLevel, {
-						[styles.thirdLevelActive]: true
-					})}>
-						{p.category}
-					</a>
-					))
+			pages.map(p => (
+				// eslint-disable-next-line react/jsx-key
+				<motion.div key={p._id} variants={variantsCildren}>
+					
+						<a href={`/${route}/${p.alias}`} className={cn(styles.thirdLevel, {
+							[styles.thirdLevelActive]: `/${route}/${p.alias}` == router.asPath
+						})}>
+							{p.category}
+						</a>
+					
+				</motion.div>
+			))
 		);
 	};
 
